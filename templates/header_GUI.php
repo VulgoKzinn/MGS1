d5cd835fa45723d2000e31555e6ac1de856f31dd
Guilherme-SQ <gk181796@gmail.com>
2026-03-25 20:04:03 -0300

    add submenu
    ok

--- templates/header_GUI.php
+++ templates/header_GUI.php
@@ -1,4 +1,18 @@
-<?php require_once "modal_post.php"; ?>
+<?php require_once "modal_post.php"; 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/headerGui.css">
+</head>
+<body>
+    
+</body>
+</html>
    <header>
        <nav>
            <img src="img/logo.svg" alt="logo" class="logo">
@@ -11,7 +25,19 @@
                </li>
                <li><a href="#">VAGAS PREENCHIDAS</a></li>
            </ul>
-           <a href="login.php" class="user-icon"><i class="fa-solid fa-user"></i></a>
+           <div class="dropdown">
+    <a href="#" class="user-icon" data-bs-toggle="dropdown">
+        <i class="fa-solid fa-user"></i>
+    </a>
+
+    <ul class="dropdown-menu dropdown-menu-end">
+        <li><a href="./perfil.php" class="dropdown-item">Perfil</a></li>
+        <li><a href="" class="dropdown-item">Notificações</a></li>
+        <li><a href="" class="dropdown-item">Configurações</a></li>
+        <li><hr class="dropdown-divider"></li>
+        <li><a href="./login.php" class="dropdown-item text-danger">Sair</a></li>
+    </ul>
+</div>
        </nav>
    </header>
 
