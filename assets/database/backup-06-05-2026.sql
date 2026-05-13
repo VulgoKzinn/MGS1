2330118436db665f6913175dccba40db17c1a4e0
Sa-Donato <sabrinasdonato@gmail.com>
2026-05-12 22:28:20 -0300

    backup do banco

--- assets/database/backup-06-05-2026.sql
+++ assets/database/backup-06-05-2026.sql
@@ -1,5 +1,5 @@
 -- --------------------------------------------------------
--- Servidor:                     10.97.46.111
+-- Servidor:                     10.97.46.104
 -- Versão do servidor:           10.4.32-MariaDB - mariadb.org binary distribution
 -- OS do Servidor:               Win64
 -- HeidiSQL Versão:              12.14.0.7165
@@ -48,17 +48,11 @@ CREATE TABLE IF NOT EXISTS `tb_empresa` (
   PRIMARY KEY (`id`),
   KEY `FK_tb_empresa_tb_login` (`id_login`),
   CONSTRAINT `FK_tb_empresa_tb_login` FOREIGN KEY (`id_login`) REFERENCES `tb_login` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Copiando dados para a tabela db_mgs.tb_empresa: ~6 rows (aproximadamente)
+-- Copiando dados para a tabela db_mgs.tb_empresa: ~1 rows (aproximadamente)
 INSERT INTO `tb_empresa` (`id`, `id_login`, `nome_fantasia`, `rzsocial`, `telefone`, `complemento`, `cnpj`, `cep`, `atuacao`, `numero`, `ativo`, `data_cadastro`) VALUES
-	(8, 13, 'Sport', 'Sport', '2435892', 'Estádio', '0942429529', '49258395', '4', '25', 1, '2026-04-29 21:40:39'),
-	(9, 14, 'Futebol', 'Futebol', '16370396', 'Estádio', '473747483', '3969396', '4', '99', 1, '2026-04-29 21:41:30'),
-	(10, 19, 'Teste', 'teste', '3456787654', 'Casa', '234567', '456788765', '5', '45678765', 1, '2026-04-29 22:25:49'),
-	(11, 20, 'Teste300', 'teste9999', '23456787654', 'Casa', '3456787654', '2345678765', '7', '70', 1, '2026-04-29 22:32:36'),
-	(13, 27, '', 'teste', '(45) 67765-', 'Caasa', '34.567.876/5', '34567-887', '3', '2345', 1, '2026-05-04 21:42:59'),
-	(14, 29, 'Testenovo1', 'Testenovo1', '(34) 5678', 'Casa', '23.456.7', '23456-7', '3', '2345', 1, '2026-05-04 21:51:40'),
-	(15, 30, 'testeteste', 'testeteste', '(12) 34567-', 'Casa', '23.456.789/0', '23456-789', '4', '1234', 1, '2026-05-04 22:09:08');
+	(17, 36, 'LanziTech Informatica', 'LanziTech', '(78) 26756-', 'Sala', '12.165.482/948', '72867-888', '1', '30', 1, '2026-05-11 21:46:22');
 
 -- Copiando estrutura para tabela db_mgs.tb_formacoes
 CREATE TABLE IF NOT EXISTS `tb_formacoes` (
@@ -73,18 +67,50 @@ CREATE TABLE IF NOT EXISTS `tb_formacoes` (
 
 -- Copiando dados para a tabela db_mgs.tb_formacoes: ~0 rows (aproximadamente)
 
--- Copiando estrutura para tabela db_mgs.tb_img_perfil
-CREATE TABLE IF NOT EXISTS `tb_img_perfil` (
+-- Copiando estrutura para tabela db_mgs.tb_img_capa_empresa
+CREATE TABLE IF NOT EXISTS `tb_img_capa_empresa` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `imagem` varchar(100) NOT NULL,
-  `id_perfil` int(11) NOT NULL,
+  `id_perfil_empresa` int(11) NOT NULL,
   `data_cadastro` datetime NOT NULL DEFAULT current_timestamp(),
   `ativo` tinyint(1) NOT NULL DEFAULT 1,
   `padrao` tinyint(1) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `id_perfil_empresa` (`id_perfil_empresa`),
+  CONSTRAINT `FK_tb_img_capa_empresa_tb_perfil_empresa` FOREIGN KEY (`id_perfil_empresa`) REFERENCES `tb_perfil_empresa` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC;
+
+-- Copiando dados para a tabela db_mgs.tb_img_capa_empresa: ~0 rows (aproximadamente)
+
+-- Copiando estrutura para tabela db_mgs.tb_img_foto_empresa
+CREATE TABLE IF NOT EXISTS `tb_img_foto_empresa` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `imagem` varchar(100) NOT NULL,
+  `id_perfil_empresa` int(11) NOT NULL,
+  `data_cadastro` datetime NOT NULL DEFAULT current_timestamp(),
+  `ativo` tinyint(1) NOT NULL DEFAULT 1,
+  `padrao` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY `id_perfil_empresa` (`id_perfil_empresa`),
+  CONSTRAINT `FK_tb_img_foto_empresa_tb_perfil_empresa` FOREIGN KEY (`id_perfil_empresa`) REFERENCES `tb_perfil_empresa` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC;
+
+-- Copiando dados para a tabela db_mgs.tb_img_foto_empresa: ~0 rows (aproximadamente)
+
+-- Copiando estrutura para tabela db_mgs.tb_img_perfil_empresa
+CREATE TABLE IF NOT EXISTS `tb_img_perfil_empresa` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `imagem` varchar(100) NOT NULL,
+  `id_perfil_empresa` int(11) NOT NULL,
+  `data_cadastro` datetime NOT NULL DEFAULT current_timestamp(),
+  `ativo` tinyint(1) NOT NULL DEFAULT 1,
+  `padrao` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY `id_perfil_empresa` (`id_perfil_empresa`),
+  CONSTRAINT `FK_tb_img_perfil_empresa_tb_perfil_empresa` FOREIGN KEY (`id_perfil_empresa`) REFERENCES `tb_perfil_empresa` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Copiando dados para a tabela db_mgs.tb_img_perfil: ~0 rows (aproximadamente)
+-- Copiando dados para a tabela db_mgs.tb_img_perfil_empresa: ~0 rows (aproximadamente)
 
 -- Copiando estrutura para tabela db_mgs.tb_img_vaga
 CREATE TABLE IF NOT EXISTS `tb_img_vaga` (
@@ -97,13 +123,18 @@ CREATE TABLE IF NOT EXISTS `tb_img_vaga` (
   PRIMARY KEY (`id`) USING BTREE,
   KEY `FK_tb_img_vaga_tb_vagas` (`id_vaga`) USING BTREE,
   CONSTRAINT `FK_tb_img_vaga_tb_vagas` FOREIGN KEY (`id_vaga`) REFERENCES `tb_vagas` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Copiando dados para a tabela db_mgs.tb_img_vaga: ~1 rows (aproximadamente)
+-- Copiando dados para a tabela db_mgs.tb_img_vaga: ~8 rows (aproximadamente)
 INSERT INTO `tb_img_vaga` (`id`, `imagem`, `data_cadastro`, `ativo`, `padrao`, `id_vaga`) VALUES
-	(11, '3c9b358ebe72c480cfe3818268bc2af7.png', '2026-05-05 22:23:17', 1, 0, 22),
-	(12, '3ed87075a0d1435fddaf256947b02432.jpg', '2026-05-05 22:26:22', 1, 0, 23),
-	(13, 'a5b803b6ea3ddad700866d0856c8cb08.png', '2026-05-05 22:31:15', 1, 0, 24);
+	(42, '563c66e772e826b4fc1ceb1bddef5616.png', '2026-05-11 21:46:52', 1, 0, 47),
+	(43, 'c80e1cb9f90318d8c2f648c749cdab88.jpg', '2026-05-11 22:00:09', 1, 0, 48),
+	(44, '81101f8313c01c9d344a92a7915d0c0d.png', '2026-05-11 22:05:47', 1, 0, 52),
+	(45, 'b1d36699dc46b3f1652f72eaf92dd38d.png', '2026-05-11 22:10:23', 1, 0, 53),
+	(46, 'ba23a898e7037e41ae41c031c0b05dc9.png', '2026-05-12 19:35:40', 1, 0, 54),
+	(47, 'b198e1c10670270aadebbf4f12843937.png', '2026-05-12 19:37:42', 1, 0, 55),
+	(48, '8a1ab8c6f377ec7b2ae08b9a7de5aa65.png', '2026-05-12 19:49:20', 1, 0, 56),
+	(49, '2e7219383ddd7493c2857ff975a163b2.png', '2026-05-12 19:50:38', 1, 0, 57);
 
 -- Copiando estrutura para tabela db_mgs.tb_login
 CREATE TABLE IF NOT EXISTS `tb_login` (
@@ -118,28 +149,13 @@ CREATE TABLE IF NOT EXISTS `tb_login` (
   UNIQUE KEY `email` (`email`),
   KEY `FK_tb_login_tb_nivel` (`id_nivel`),
   CONSTRAINT `FK_tb_login_tb_nivel` FOREIGN KEY (`id_nivel`) REFERENCES `tb_nivel` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Copiando dados para a tabela db_mgs.tb_login: ~16 rows (aproximadamente)
+-- Copiando dados para a tabela db_mgs.tb_login: ~3 rows (aproximadamente)
 INSERT INTO `tb_login` (`id`, `email`, `senha`, `data_cadastro`, `ativo`, `s_temp`, `id_nivel`) VALUES
-	(1, 'kaualanzieri@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$QXUyL1BpcTVMcFUwLmdiVQ$Adg7SmEZ0eIJtJAiv26GAXe/PUEpdajFBF9uKImw7ZU', '2026-04-06 19:14:32', 1, 0, 2),
-	(2, 'admin@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$TUpqL2pGSVBUTnhlc01pWg$q8XzEsYNLRJxObhE+yq56RwMxyP5ynjgaQFMm9i8J9Q', '2026-04-07 19:55:43', 1, 0, 1),
-	(4, 'pmgs@hotmail.com', '$argon2id$v=19$m=65536,t=4,p=1$UllKUWhTQWE0OUd1UElEcA$DxDUy1mvo25fp42UnlgajQX7VV0gU8J78yJt9b+u/uc', '2026-04-29 20:24:07', 1, 0, 2),
-	(13, 'SantosFC@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$eld1VUhCNUpJNERtbTdvVw$2YmJCYyahnSvD6L9SNAW/0z23e7Y7XwM7h/F9942Hhk', '2026-04-29 21:40:08', 1, 0, 1),
-	(14, 'CorinthiansSCCP@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$dUtOanJwV25qU0RJWVh6eQ$/l+IFC23FlGsovhK44A9lhSniRzONzKjr6vHcCDC/Iw', '2026-04-29 21:41:05', 1, 0, 1),
-	(15, 'teste99@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$clFmcjE3UXBQWk52bFV0Zg$ovtkE+h56qPxK7K834m0GLZLA4/+Xn/Hg4zNQKc30nE', '2026-04-29 21:57:50', 1, 0, 2),
-	(16, 'teste50@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$bG9EMnZBWVJpVkhHRjFOaA$5u23iWSzJ6rw0Cpy6P/XjwyG1D2ZMSYnJAU7TUnzFns', '2026-04-29 22:17:37', 1, 0, 2),
-	(17, 'teste53@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$WlduaXlDZ2FjejlrNm5jVA$YaXseM/GAephbdIO3tVs5NRd3G27L8Nb79VFTdoV42g', '2026-04-29 22:22:52', 1, 0, 2),
-	(18, 'teste80@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$azZVeVpzS25VMUp4LlpPRQ$C25dO5eF124C2q1PvWbiznD4281MyyscP98WP3o8cP8', '2026-04-29 22:24:38', 1, 0, 2),
-	(19, 'testeEmp@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$ZG9rQVZnU2pHWldRV1FEWQ$rkpOjsKCE9g9CCUUXdJCvCUAxZSaotjlan5SE5LHLis', '2026-04-29 22:25:29', 1, 0, 1),
-	(20, 'teste999@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$MDg3NU9TTi9HTjdaMG5zVg$GMd3gtwKGAo4N7VAIPuvf6dWyFu2ua6IGO2Kx45E7oM', '2026-04-29 22:32:20', 1, 0, 1),
-	(24, 'guilherme@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$MkV4dHNBdzdtVXB6WlJjVg$iyPbWdW/ovYxgr2RyK2kv6erhr15RVLJGLWBKWj27pc', '2026-05-04 19:56:53', 1, 0, 2),
-	(27, 'teste300@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$ekhHZ21CbzVSV2ZyVm1XMw$E7PdTYIIkBSN5vmMmRiG2kwtYgDHRj5IOcCLuc6fT7Q', '2026-05-04 21:42:47', 1, 0, 1),
-	(28, 'testenovo1@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$M0QwQ3BORVI4eHpYSlpWZQ$6uUQMY68lkZS7lvYq8Wu1VsuIIghxHWoXqaGaz2VY7k', '2026-05-04 21:46:21', 1, 0, 1),
-	(29, 'testenovo2@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$UlhIQ3ZSWHBnT1hCNWdndA$mEjN5p5F63E91EizGPgKyYwnIxARHPT5nS+8M6ldlWg', '2026-05-04 21:51:18', 1, 0, 1),
-	(30, 'teste777@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$cWVRNGxlT2d4TzR4Z0Y3Mg$QsveHwGcbJw+rCQmCX/e5Wudtbx4Yxk96nuaBFYiMic', '2026-05-04 22:08:49', 1, 0, 1),
-	(31, 'guilhermeUSU@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$UXFwZDJac05rNHFpb1NPZw$AdjE20UwRonVuSJst+EYqkUHLF6ouvVmNsP43FC7zZc', '2026-05-05 19:22:17', 1, 0, 2),
-	(32, 'PedroUSU@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$YVZldEJHSDFPZm42NjQ2WA$agQNW7q1RwOYWDJKbb1vD/h4fX7P5f4CHxUpMeMy9zc', '2026-05-05 21:47:13', 1, 0, 2);
+	(35, 'admin@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$TTl5cjl1elc3dHQwaHQveg$tzl/Unc+3A5iR4oJFMsSu6J01eGhYRJjHh4Mxfp7+m8', '2026-05-11 21:45:24', 1, 0, 2),
+	(36, 'empresa@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$blRaWGk1Zk5RdW9yYUFuMg$s0JRe1fHDfT6TQGglM/Ut3G7UlAR9H1nhnhzy0oWyW4', '2026-05-11 21:45:49', 1, 0, 1),
+	(37, 'pmgs@hotmail.com', '$argon2id$v=19$m=65536,t=4,p=1$YXVoTU96bFV2VGtDOGRkSQ$HtkjchjuLHBEf0K8CbeN87mNf59RWG15GllxPD2ukw8', '2026-05-11 21:47:51', 1, 0, 2);
 
 -- Copiando estrutura para tabela db_mgs.tb_nivel
 CREATE TABLE IF NOT EXISTS `tb_nivel` (
@@ -155,20 +171,42 @@ INSERT INTO `tb_nivel` (`id`, `nivel`, `data_cadastro`, `ativo`) VALUES
 	(1, 'empresa', '2026-04-06 19:28:19', 1),
 	(2, 'usuario', '2026-04-06 19:28:32', 1);
 
+-- Copiando estrutura para tabela db_mgs.tb_perfil_candidato
+CREATE TABLE IF NOT EXISTS `tb_perfil_candidato` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `telefone` varchar(20) DEFAULT NULL,
+  `endereco` varchar(255) DEFAULT NULL,
+  `sobre` text DEFAULT NULL,
+  `cargo` varchar(255) DEFAULT NULL,
+  `experiencia` text DEFAULT NULL,
+  `projeto` text DEFAULT NULL,
+  `certificado` text DEFAULT NULL,
+  `id_foto_perfil` varchar(255) DEFAULT NULL,
+  `id_foto_banner` varchar(255) DEFAULT NULL,
+  `data_cadastro` datetime DEFAULT current_timestamp(),
+  `ativo` tinyint(1) DEFAULT 1,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Copiando dados para a tabela db_mgs.tb_perfil_candidato: ~2 rows (aproximadamente)
+INSERT INTO `tb_perfil_candidato` (`id`, `nome`, `email`, `telefone`, `endereco`, `sobre`, `cargo`, `experiencia`, `projeto`, `certificado`, `id_foto_perfil`, `id_foto_banner`, `data_cadastro`, `ativo`) VALUES
+	(1, 'pedro', 'pedro1234@hotmail.com', '19989263916', 'Águas da Prata', 'lixo', 'lixeiro', 'dkvnzvz', 'vdn\\dzlknkz', 'afgsgson', NULL, NULL, '2026-05-06 20:54:08', 1),
+	(2, 'lara', 'lara@gmail.com', '189899999', 'Águas da Prata', 'jdiadhPAD', 'atendente', 'IHDAPHaaDJ', '', 'gestao de pessoas', NULL, 'b789c96a331273d6310aba57345f714a.png', '2026-05-06 21:13:52', 1),
+	(3, 'julia', 'julia@gmail.com', '19989263916', 'Águas da Prata', 'oii', 'atendente', 'fjsihgishgs', 'sgjsihspgks', 'cert_e78a43db88455e8021df2af7c37a15e5.png', '910d56c0f588032b03008391258bc865.png', '35abedf28f74f4ee49a8f4bccfb0285e.png', '2026-05-11 20:27:59', 1);
+
 -- Copiando estrutura para tabela db_mgs.tb_perfil_empresa
 CREATE TABLE IF NOT EXISTS `tb_perfil_empresa` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(255) NOT NULL,
+  `id_empresa` int(11) DEFAULT NULL,
+  `slogan` varchar(255) NOT NULL,
+  `quem_somos` varchar(255) NOT NULL,
   `data_cadastro` datetime DEFAULT current_timestamp(),
   `ativo` tinyint(1) DEFAULT 1,
-  `id_img_perfil` int(11) DEFAULT NULL,
-  `id_img_capa` int(11) DEFAULT NULL,
-  `descricao` text DEFAULT NULL,
-  `id_img_sobre` int(11) DEFAULT NULL,
-  `site` varchar(255) DEFAULT NULL,
-  `instagram` varchar(255) DEFAULT NULL,
-  `facebook` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `id_empresa` (`id_empresa`),
+  CONSTRAINT `FK_tb_perfil_empresa_tb_empresa` FOREIGN KEY (`id_empresa`) REFERENCES `tb_empresa` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Copiando dados para a tabela db_mgs.tb_perfil_empresa: ~0 rows (aproximadamente)
@@ -239,6 +277,7 @@ INSERT INTO `tb_users` (`id`, `nome`, `cargo`, `email`, `telefone`, `biografia`,
 -- Copiando estrutura para tabela db_mgs.tb_vagas
 CREATE TABLE IF NOT EXISTS `tb_vagas` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_empresa` int(11) NOT NULL,
   `vaga` varchar(255) NOT NULL,
   `area_atuacao` varchar(255) DEFAULT NULL,
   `modalidade` varchar(100) DEFAULT NULL,
@@ -251,14 +290,21 @@ CREATE TABLE IF NOT EXISTS `tb_vagas` (
   `data_cadastro` datetime DEFAULT current_timestamp(),
   `ativo` tinyint(1) DEFAULT 1,
   `modelo_de_trabalho` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  PRIMARY KEY (`id`),
+  KEY `FK_tb_vagas_tb_empresa` (`id_empresa`),
+  CONSTRAINT `FK_tb_vagas_tb_empresa` FOREIGN KEY (`id_empresa`) REFERENCES `tb_empresa` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=58 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Copiando dados para a tabela db_mgs.tb_vagas: ~1 rows (aproximadamente)
-INSERT INTO `tb_vagas` (`id`, `vaga`, `area_atuacao`, `modalidade`, `localizacao`, `descricao`, `requisitos`, `salario`, `beneficio`, `carga_horaria`, `data_cadastro`, `ativo`, `modelo_de_trabalho`) VALUES
-	(22, 'TESTEEEEEEE', 'TESTEEEEEEE', 'Home Office', 'TESTEEEEEEE', 'TESTEEEEEEE', 'TESTEEEEEEE', 0.00, 'TESTEEEEEEE', 'TESTEEEEEEE', '2026-05-05 22:23:17', 1, 'Presencial'),
-	(23, 'testeeeeeeeeeeeee', 'testeeeeeeeeeeeee', 'Home Office', 'testeeeeeeeeeeeee', 'testeeeeeeeeeeeee', 'testeeeeeeeeeeeee', 0.00, 'testeeeeeeeeeeeee', 'testeeeeeeeeeeeee', '2026-05-05 22:26:22', 1, 'Presencial'),
-	(24, 'testeeeeeeee', 'teste', 'Presencial', 'testeeeeeeee', 'testeeeeeeee', 'testeeeeeeee', 0.00, 'testeeeeeeee', 'testeeeeeeee', '2026-05-05 22:31:15', 1, 'Home Office');
+-- Copiando dados para a tabela db_mgs.tb_vagas: ~8 rows (aproximadamente)
+INSERT INTO `tb_vagas` (`id`, `id_empresa`, `vaga`, `area_atuacao`, `modalidade`, `localizacao`, `descricao`, `requisitos`, `salario`, `beneficio`, `carga_horaria`, `data_cadastro`, `ativo`, `modelo_de_trabalho`) VALUES
+	(47, 17, 'Tecnico TI', 'TI', 'Presencial', 'São paulo', 'tete', 'tete', 1200.00, 'Vale alimentação', '80', '2026-05-11 21:46:52', 1, 'Presencial'),
+	(48, 17, 'Teste', 'Teste', 'Presencial', 'Sj', '/', '/', 1200.00, 'tudo', '1200 semanal', '2026-05-11 22:00:09', 1, 'Presencial'),
+	(52, 17, 'sa', 'sa', 'Híbrido', 'sa', 'sa', 'sa', 0.00, 'sa', 'sa', '2026-05-11 22:05:47', 1, 'Híbrido'),
+	(53, 17, 'sabrina', 'sabrina', 'Home Office', 'sabrina', 'sabrina', 'sabrina', 0.00, 'sabrina', 'sabrina', '2026-05-11 22:10:23', 1, 'Presencial'),
+	(54, 17, 'teste05', 'teste05', 'Home Office', 'teste05', 'teste05', 'teste05', 0.00, 'teste05', 'teste05', '2026-05-12 19:35:40', 1, 'Home Office'),
+	(55, 17, 'Teste', 'Teste', 'Presencial', 'Sj', 'tudo', 'tudo', 1200.00, 'tudo', '1200 semanal', '2026-05-12 19:37:42', 1, 'Presencial'),
+	(56, 17, 'sabrina', 'sabrina', 'Home Office', 'sabrina', 'sabrina', 'sabrina', 0.00, 'sabrina', 'sabrina', '2026-05-12 19:49:20', 1, 'Presencial'),
+	(57, 17, 'donato', 'donato', 'Híbrido', 'donato', 'donato', 'donato', 0.00, 'donato', 'donato', '2026-05-12 19:50:38', 1, 'Presencial');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
